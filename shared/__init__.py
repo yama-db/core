d3926942214103a08724fc2d5d8f289861ff69b3
Anineco Nyanta <anineco@nifty.com>
2026-03-03 22:53:37 +0900

    add EPS

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,3 +1,4 @@
 __version__ = "1.0.0"
 from .generate_source_uuid import generate_source_uuid
 from .extract_aliases import extract_aliases
+from .config import EPS
---
+++ shared/config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+EPS = 40  # 位置の許容誤差[m]
+
+# __END__
