e7b74ae413b347c98be58edc0d19f6096987eb4a
Anineco Nyanta <anineco@nifty.com>
2026-07-08 06:11:20 +0900

    improve legacy

--- data_sources/legacy/dump.py
+++ data_sources/legacy/dump.py
@@ -3,75 +3,76 @@
 
 import csv
 import sys
-from pathlib import Path
 
-import mysql.connector
-
-from shared import generate_source_uuid
+from shared import db_util
 
 # MySQL接続の確立
+conn = None
+cursor = None
+success = False
+
 try:
-    my_cnf = Path(sys.prefix).parent / "legacy.my.cnf"
-    conn = mysql.connector.connect(
-        option_files=str(my_cnf),
-        option_groups=["client"],
-        autocommit=False,
+    conn, cursor = db_util.db_open(config_file="legacy.my.cnf")
+
+    fieldnames = [
+        "raw_id",
+        "raw_type",
+        "name",
+        "kana",
+        "lat",
+        "lon",
+        "elevation",
+        "z_min",
+        "last_updated_at",
+    ]
+    writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+    writer.writeheader()
+
+    raw_type_list = [
+        "等高線",
+        "その他",
+        "標高点",
+        "四等三角点",
+        "三等三角点",
+        "二等三角点",
+        "一等三角点",
+        "電子基準点",
+    ]
+
+    z_min_list = [13, 13, 12, 11, 10, 9, 8, 8]
+
+    cursor.execute(
+        """
+        SELECT g.id AS raw_id, s.name, s.kana, lat, lon, alt, level
+        FROM geom AS g
+        RIGHT JOIN sanmei AS s USING (id)
+        WHERE g.id IS NOT NULL AND type >= 1
+        ORDER BY g.id, type
+        """,
     )
-    cursor = conn.cursor(dictionary=True)
-except mysql.connector.Error as err:
-    print(f"MySQL Error: {err}")
-    sys.exit(1)
+    for row in cursor.fetchall():
+        level = row["level"]
+        writer.writerow(
+            {
+                "raw_id": row["raw_id"],
+                "raw_type": raw_type_list[level & 7],
+                "name": row["name"],
+                "kana": row["kana"],
+                "lat": row["lat"],
+                "lon": row["lon"],
+                "elevation": row["alt"],
+                "z_min": z_min_list[(level >> 3) & 7],
+                "last_updated_at": None,
+            }
+        )
 
-fieldnames = [
-    "raw_id",
-    "raw_type",
-    "name",
-    "kana",
-    "lat",
-    "lon",
-    "elevation",
-    "z_min",
-    "last_updated_at",
-]
-writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
-writer.writeheader()
+    success = True
 
-raw_type_list = [
-    "等高線",
-    "その他",
-    "標高点",
-    "四等三角点",
-    "三等三角点",
-    "二等三角点",
-    "一等三角点",
-    "電子基準点",
-]
-
-z_min_list = [13, 13, 12, 11, 10, 9, 8, 8]
-
-cursor.execute(
-    """
-    SELECT g.id AS raw_id, s.name, s.kana, lat, lon, alt, level
-    FROM geom AS g
-    RIGHT JOIN sanmei AS s USING (id)
-    WHERE g.id IS NOT NULL AND type >= 1
-    ORDER BY g.id, type
-    """,
-)
-for row in cursor.fetchall():
-    grade = (row["level"] >> 3) & 7
-    writer.writerow(
-        {
-            "raw_id": row["raw_id"],
-            "raw_type": raw_type_list[grade],
-            "name": row["name"],
-            "kana": row["kana"],
-            "lat": row["lat"],
-            "lon": row["lon"],
-            "elevation": row["alt"],
-            "z_min": z_min_list[grade],
-            "last_updated_at": None,
-        }
-    )
+except Exception as err:
+    print(f"Error during DB session: {err}", file=sys.stderr)
+    raise
+finally:
+    if conn or cursor:
+        db_util.db_close(conn, cursor, success=success)
 
 # __END__
